783754164c30b61621d24a6400a00432647aa219
kdastation <monnicmambo2281337@gmail.com>
2022-06-21 15:10:28 +0300

    Create Cards Component.

---
+++ src/components/Cards.vue
@@ -0,0 +1,16 @@
+<template>
+  <Card v-for="product in products" :key="product.id" :product="product" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { IProduct } from "@/types/models/product";
+import Card from "@/components/Card.vue";
+
+const products = ref<IProduct[]>([
+  { id: 1, price: 2000000, name: "Ведьмак 3", img: "img" },
+  { id: 2, price: 200000, name: "Ведьмак 2", img: "img" },
+]);
+</script>
+
+<style scoped></style>
