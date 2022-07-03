ae6ec09c5f455f9699ec49540335103b17cdbdb3
kdastation <monnicmambo2281337@gmail.com>
2022-07-03 13:16:46 +0300

    Create Categories product.

---
+++ src/components/categories/Categories.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="top_sellers_categories">
+    <ul class="top_sellers_categories__list">
+      <Category
+        v-for="category in categories"
+        :key="category.id"
+        :category="category"
+        :is-active="category.id === idActiveCategory"
+        @changeCategory="changeCategory"
+      />
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import { defineEmits, defineProps } from "vue";
+import { ICategory } from "@/types/models/category";
+import Category from "@/components/categories/category/Category.vue";
+interface CategoriesProps {
+  categories: ICategory[];
+  idActiveCategory: number;
+}
+interface CategoriesEmits {
+  (e: "changeCategory", id: number): void;
+}
+
+const props = defineProps<CategoriesProps>();
+const emit = defineEmits<CategoriesEmits>();
+const changeCategory = (id: number) => {
+  console.log(id);
+  emit("changeCategory", id);
+};
+</script>
+
+<style scoped lang="scss">
+.top_sellers_categories {
+  margin-bottom: 6.25rem;
+  &__list {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
+}
+.container {
+  max-width: 1353px;
+  padding: 0 10px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/categories/category/Category.vue
@@ -0,0 +1,30 @@
+<template>
+  <li
+    class="top_sellers_categories__item"
+    @click="emit('changeCategory', category.id)"
+  >
+    <GreenCategoryButton :is-active="isActive">{{
+      category.name
+    }}</GreenCategoryButton>
+  </li>
+</template>
+
+<script lang="ts" setup>
+import GreenCategoryButton from "@/ui-components/GreenCategoryButton.vue";
+import { defineEmits, defineProps } from "vue";
+import { ICategory } from "@/types/models/category";
+interface CategoryProps {
+  category: ICategory;
+  isActive: boolean;
+}
+
+interface CategoryEmits {
+  (e: "changeCategory", id: number): void;
+}
+
+const props = defineProps<CategoryProps>();
+const emit = defineEmits<CategoryEmits>();
+console.log(props.category.id, props.isActive);
+</script>
+
+<style scoped lang="scss"></style>
