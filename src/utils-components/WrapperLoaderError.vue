7c801c1e0a234a46965b2506156cc2d314e4c31d
kdastation <monnicmambo2281337@gmail.com>
2022-07-03 13:20:15 +0300

    Create utils components.

---
+++ src/utils-components/WrapperLoaderError.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="isLoading">Loading...</div>
+  <div v-if="isError">Произошла ошибка</div>
+  <slot v-if="!isLoading && !isError"></slot>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+interface WrapperLoaderErrorProps {
+  isLoading: boolean;
+  isError: boolean;
+}
+
+const props = defineProps<WrapperLoaderErrorProps>();
+</script>
+
+<style scoped></style>
