ca50acaa37b18884df175e4a95b9bcc15e1adb84
kdastation <monnicmambo2281337@gmail.com>
2022-07-03 13:20:30 +0300

    Create social component-block.

---
+++ src/components/Social.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="social">
+    <li class="social__item">
+      <a class="social__link" href="#"
+        ><img
+          class="social__image"
+          src="../assets/facebook-icon.svg"
+          alt="facebook"
+      /></a>
+    </li>
+    <li class="social__item">
+      <a class="social__link" href="#"
+        ><img
+          class="social__image"
+          src="../assets/twitter-icon.svg"
+          alt="twitter"
+      /></a>
+    </li>
+    <li class="social__item">
+      <a class="social__link" href="#"
+        ><img
+          class="social__image"
+          src="../assets/instagram-icon.svg"
+          alt="instagram"
+      /></a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="scss">
+.social {
+  display: flex;
+  gap: 2.5rem;
+  align-items: center;
+}
+</style>
